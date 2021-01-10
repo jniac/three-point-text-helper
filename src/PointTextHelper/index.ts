03cc6fd0fdaa2451867e3e3b10f42c007edcc67e
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2021-01-10 17:15:29 +0100

    'displayVertices' is ok (and very efficient)

--- src/PointTextHelper/index.ts
+++ src/PointTextHelper/index.ts
@@ -7,11 +7,16 @@ import { get_char_offset, get_count_and_offsets } from '../atlas-utils.js'
 
 const CHAR_MAX_LIMIT = 12
 
-const defaultDisplay = {
+const defaultDisplayParams = {
   position: { x:0, y:0, z:0 }, 
   color: 'white', 
   size: 1,
   text: 'foo',
+}
+
+const defaultDisplayVerticesParams = {
+  color: 'white',
+  size: 1,
 }
 
 class PointTextHelper extends THREE.Points {
@@ -27,12 +32,12 @@ class PointTextHelper extends THREE.Points {
 
     const geometry = new THREE.BufferGeometry()
 
-    geometry.setAttribute('position', new THREE.Float32BufferAttribute([], 3))
-    geometry.setAttribute('color', new THREE.Float32BufferAttribute([], 3))
-    geometry.setAttribute('size', new THREE.Float32BufferAttribute([], 1))
-    geometry.setAttribute('char_count', new THREE.Float32BufferAttribute([], 1))
+    geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(0), 3))
+    geometry.setAttribute('color', new THREE.BufferAttribute(new Float32Array(0), 3))
+    geometry.setAttribute('size', new THREE.BufferAttribute(new Float32Array(0), 1))
+    geometry.setAttribute('char_count', new THREE.BufferAttribute(new Float32Array(0), 1))
     for (let i = 0; i < charMax; i++) {
-      geometry.setAttribute(`char_offset_${i}`, new THREE.Float32BufferAttribute([], 2))
+      geometry.setAttribute(`char_offset_${i}`, new THREE.BufferAttribute(new Float32Array(0), 2))
     }
     
     const material = get_material(charMax)
@@ -43,82 +48,100 @@ class PointTextHelper extends THREE.Points {
     this.charMax = charMax
   }
 
-  private updateAttributes(position:number[], color:number[], size:number[]) {
-
-      const geometry = this.geometry as THREE.BufferGeometry
-      geometry.setAttribute('position', new THREE.Float32BufferAttribute(position, 3))
-      geometry.setAttribute('color', new THREE.Float32BufferAttribute(color, 3))
-      geometry.setAttribute('size', new THREE.Float32BufferAttribute(size, 1))
-      geometry.setAttribute('char_offset_X', new THREE.Float32BufferAttribute(size.flatMap((v, i) => get_char_offset(i)), 2).setUsage(THREE.DynamicDrawUsage))
+  private push(name:string, numbers:ArrayLike<number>) {
+    const geometry = this.geometry as THREE.BufferGeometry
+    const buffer = geometry.getAttribute(name)
+    const array = new Float32Array(buffer.array.length + numbers.length)
+    array.set(buffer.array, 0)
+    array.set(numbers, buffer.array.length)
+    geometry.setAttribute(name, new THREE.BufferAttribute(array, buffer.itemSize))
   }
 
-  display(params:Partial<typeof defaultDisplay> = defaultDisplay) {
+  private pushFill(name:string, value:number, count:number) {
+    const geometry = this.geometry as THREE.BufferGeometry
+    const buffer = geometry.getAttribute(name)
+    const array = new Float32Array(buffer.array.length + count)
+    array.set(buffer.array, 0)
+    array.fill(value, buffer.array.length)
+    geometry.setAttribute(name, new THREE.BufferAttribute(array, buffer.itemSize))
+  }
 
-    params = { ...defaultDisplay,  ...params }
+  display(params:Partial<typeof defaultDisplayParams> = defaultDisplayParams) {
 
-    const push = (name:string, ...numbers:number[]) => {
-      const geometry = this.geometry as THREE.BufferGeometry
-      const buffer = geometry.getAttribute(name)
-      const array = [...buffer.array as Float32Array]
-      array.push(...numbers)
-      geometry.setAttribute(name, new THREE.Float32BufferAttribute(array, buffer.itemSize))
-    }
+    params = { ...defaultDisplayParams,  ...params }
 
     const { x, y, z } = params.position
-    push('position', x, y, z)
+    this.push('position', [x, y, z])
     
     const { r, g, b } = new THREE.Color(params.color)
-    push('color', r, g, b)
+    this.push('color', [r, g, b])
     
-    push('size', params.size)
+    this.push('size', [params.size])
 
     const { charMax } = this
     const { count, offsets } = get_count_and_offsets(params.text, charMax)
-    push('char_count', count)
+    this.push('char_count', [count])
 
     for (let i = 0; i < charMax; i++) {
-      push(`char_offset_${i}`, ...offsets[i])
+      this.push(`char_offset_${i}`, offsets[i])
     }
   }
 
-  // displayVertices(vertices:THREE.Vector3[]) {
+  displayVertices(vertices:THREE.Vector3[], {
+    color = 'white',
+    size = 1,
+    format = undefined,
+  }:{
+    size?: number,
+    color?: string | THREE.Color,
+    format?: (index:number) => string,
+  } = {}) {
+    
+    const { r, g, b } = new THREE.Color(color)
 
-  //   const position = []
-  //   const color = []
-  //   const size = []
+    const length = vertices.length
+    const { charMax } = this
+    
+    const position_array = new Float32Array(length * 3)
+    const color_array = new Float32Array(length * 3)
+    const size_array = new Float32Array(length)
+    const char_count_array = new Float32Array(length)
+    const char_offset_array = new Array<Float32Array>(charMax)
+    for (let i = 0; i < charMax; i++) {
+      char_offset_array[i] = new Float32Array(length * 2)
+    }
 
-  //   const char_count:number[] = []
-  //   const char_offset:number[][] = []
+    for (let index = 0; index < length; index++) {
 
-  //   const { charMax } = this
+      const { x, y, z } =  vertices[index]
+      position_array[index * 3 + 0] = x
+      position_array[index * 3 + 1] = y
+      position_array[index * 3 + 2] = z
 
-  //   for (let i = 0; i < charMax; i++) {
-  //     char_offset[i] = []
-  //   }
+      color_array[index * 3 + 0] = r
+      color_array[index * 3 + 1] = g
+      color_array[index * 3 + 2] = b
 
-  //   for (const [i, v] of vertices.entries()) {
-  //     position.push(v.x, v.y, v.z)
-  //     color.push(1, 1, 1)
-  //     size.push(1)
-      
-  //     const { count, indexes } = get_count_and_indexes(i, charMax)
-  //     char_count.push(count)
-  //     for (let i = 0; i < charMax; i++) {
-  //       const index = indexes[i]
-  //       const [x, y] = get_char_offset(index)
-  //       char_offset[i].push(x, y)
-  //     }
-  //   }
+      size_array[index] = size
 
-  //   const geometry = this.geometry as THREE.BufferGeometry
-  //   geometry.setAttribute('position', new THREE.Float32BufferAttribute(position, 3))
-  //   geometry.setAttribute('color', new THREE.Float32BufferAttribute(color, 3))
-  //   geometry.setAttribute('size', new THREE.Float32BufferAttribute(size, 1))
+      const text = format?.(index) ?? index.toString(10)
+      const { count, offsets } = get_count_and_offsets(text, charMax)
+      char_count_array[index] = count;
+      for (let i = 0; i < charMax; i++) {
+        const [x, y] = offsets[i]
+        char_offset_array[i][index * 2 + 0] = x
+        char_offset_array[i][index * 2 + 1] = y
+      }
+    }
 
-  //   for (let i = 0; i < charMax; i++) {
-  //     geometry.setAttribute(`char_offset_${i}`, new THREE.Float32BufferAttribute(char_offset[i], 2))
-  //   }
-  // }
+    this.push('position', position_array)
+    this.push('color', color_array)
+    this.push('size', size_array)
+    this.push('char_count', char_count_array)
+    for (let i = 0; i < charMax; i++) {
+      this.push(`char_offset_${i}`, char_offset_array[i])
+    }
+  }
 }
 
 export {
