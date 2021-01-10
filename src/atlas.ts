20a2e8c74c4c4f30b4043716402e8ab0bd915266
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2021-01-10 19:30:49 +0100

    simplier atlas

--- src/atlas.ts
+++ src/atlas.ts
@@ -1,9 +1,10 @@
 
-export const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz #@&$%?!+-_=*/\|[](){}<>.;:,×';
-export const width = 4096;
-export const height = 256;
-export const grid_width = 64;
-export const grid_height = 2;
-export const char_width = 64;
-export const char_height = 120;
-export { default as data } from './atlas-data.js';
+import atlas_data from './atlas-data'
+export const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz #@&$%?!+-_=*/\|[](){}<>.;:,×'
+export const width = 4096
+export const height = 256
+export const grid_width = 64
+export const grid_height = 2
+export const char_width = 64
+export const char_height = 120
+export const data = atlas_data
